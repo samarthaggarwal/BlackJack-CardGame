8fc53a2c22bc3c73fc97badc6143af627ba35eeb
Samarth Aggarwal <samarthaggarwal2510@gmail.com>
2019-10-04 01:50:05 +0530

    adding sarsa and q-learning

--- player.py
+++ player.py
@@ -7,7 +7,7 @@ from tqdm import tqdm
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
-np.random.seed(32)
+# np.random.seed(32)
 
 # runs an episode with given policy in the given env, returns total reward for that episode
 def episode(env, policy):
@@ -18,9 +18,6 @@ def episode(env, policy):
         state, reward, done = env.step(action)
 
     return reward
-
-def state_transformation(state):
-    return state.raw_sum + 30, state.trump_count(), state.opponent-1
 
 def monte_carlo(env, policy, first_visit, num_episodes):
     # size of v = (rawsum, number of distinct trumps, dealer's hand)
@@ -99,8 +96,6 @@ def k_step_TD(env, policy, k, alpha, num_episodes):
                 v[initial_state] += alpha * ( reward + v[final_state] - v[initial_state])
                 states = states[1:] + [copy(state)]
 
-        # TODO : states has both actionable and non-actionable states in it, DEBUG
-
         assert(states[-1]!=None), "states[-1] is None"
 
         # if states[-1] != None:
@@ -121,6 +116,122 @@ def k_step_TD(env, policy, k, alpha, num_episodes):
 
     return v
 
+def expand(states):
+    for s in states:
+        s[0].print()
+
+def k_step_sarsa(env, k, alpha, num_episodes, epsilon=None, epsilon_decay=False):
+    # size of v = (actions, rawsum, number of distinct trumps, dealer's hand)
+    q = np.zeros((61,4,10,2), dtype=float)
+    # actions = {"HIT", "STICK"}
+
+    for ep in tqdm(range(1, num_episodes+1)):
+        # print("====================== NEW EPISODE ======================")
+        # TODO : change decay rate suitably
+        episode_epsilon = epsilon/(ep**0.1) if epsilon_decay else epsilon
+        
+        states = []
+        state = env.reset()
+        state, reward, done = env.check_after_init()
+        if done:
+            # no actionable state encountered in this episode so no update
+            continue
+        # states.append(copy((state,action)))
+        
+        # take k-1 steps
+        for _ in range(k-1):
+            action = epsilon_greedy(state, q, episode_epsilon)
+            states.append((copy(state), action))
+            # print("adding state ", action)
+            # state.print()
+            state, reward, done = env.step(action)
+            if done:
+                break
+
+        if not done:
+            assert(len(states)==k-1), "number of states not correct"
+
+        if(not done):
+            while(True):
+                action = epsilon_greedy(state, q, episode_epsilon)
+                states.append((copy(state), action))
+                # print("adding state ", action)
+                # state.print()
+                state, reward, done = env.step(action)
+                if done:
+                    break
+                assert(reward==0), "reward is non-zero for intermediate states"
+                # update S_t, remove from states list and add S_t+k to the states list
+                initial_state = state_transformation(states[0][0])
+                final_state = state_transformation(state)
+                q[initial_state][0 if states[0][1]=="HIT" else 1] += alpha * ( reward + q[final_state][0 if action=="HIT" else 1] - q[initial_state][0 if states[0][1]=="HIT" else 1])
+                # print("removing state ", states[0][1])
+                # states[0][0].print()
+                states = states[1:] # + [copy((state, action))]
+
+            assert(len(states)==k), ipdb.set_trace() # "number of states in window is not k"
+            
+        assert(states[-1]!=None), "states[-1] is None"
+
+        # if states[-1] != None:
+        #     raise Exception("last state in episode is actionable, CHECK")
+        # states = states[:-1]
+
+        for s in states:
+            assert(s[0].category=="GENERAL"), "states within an episode are not actionable"
+            # if s.category=="BUST" or s.category=="SUM31":
+            #     raise Exception("states within an episode are not actionable")
+            # else:
+            #     s.print()
+            
+        # updating value of states after reaching end of episode
+        for s in states:
+            initial_state = state_transformation(s[0])
+            q[initial_state][0 if s[1]=="HIT" else 1] += alpha * ( reward - q[initial_state][0 if s[1]=="HIT" else 1]) # last state is not actionable so its value is zero
+
+    return q
+
+def q_learning(env, alpha, num_episodes, epsilon=None, epsilon_decay=False):
+    # size of v = (actions, rawsum, number of distinct trumps, dealer's hand)
+    q = np.zeros((61,4,10,2), dtype=float)
+    # actions = {"HIT", "STICK"}
+
+    for ep in tqdm(range(1, num_episodes+1)):
+        # print("====================== NEW EPISODE ======================")
+        # TODO : change decay rate suitably
+        episode_epsilon = epsilon/(ep**0.2) if epsilon_decay else epsilon
+        
+        # states = []
+        state = env.reset()
+        state, reward, done = env.check_after_init()
+        if done:
+            # no actionable state encountered in this episode so no update
+            continue
+
+        while(not done):
+            prev_state = copy(state)
+            action = epsilon_greedy(state, q, episode_epsilon)
+            state, reward, done = env.step(action)
+
+            if done:
+                break
+            assert(reward==0), "reward != 0 for actionable state"
+            assert(state.category=="GENERAL"), "states within an episode are not actionable"
+
+            # update q(s,a)
+            initial_state = state_transformation(prev_state)
+            final_state = state_transformation(state)
+            q[initial_state][0 if action=="HIT" else 1] += alpha * (reward + max(q[final_state]) - q[initial_state][0 if action=="HIT" else 1])
+
+        initial_state = state_transformation(prev_state)
+        try:
+            q[initial_state][0 if action=="HIT" else 1] += alpha * (reward - q[initial_state][0 if action=="HIT" else 1]) # last state is not actionable so its value is zero
+        except:
+            ipdb.set_trace()
+
+    return q
+
+
 env = Simulator()
 
 # # average reward for a policy
@@ -131,6 +242,14 @@ env = Simulator()
 # print(reward/num_episodes)
 
 # v = monte_carlo(env, dealer_policy, first_visit=True, num_episodes=100000)
-for k in range(1,100):
-    v = k_step_TD(env, dealer_policy, k=k, alpha=0.1, num_episodes=1000)
+
+# v = k_step_TD(env, dealer_policy, k=1, alpha=0.1, num_episodes=1000)
+
+# for k in range(1,100):
+#     v = k_step_TD(env, dealer_policy, k=k, alpha=0.1, num_episodes=1000)
+
+# for k in range(1,100):
+#         q = k_step_sarsa(env, k=k, alpha=0.1, num_episodes=1000, epsilon=0.1, epsilon_decay=True)
+
+q = q_learning(env, alpha=0.1, num_episodes=10000, epsilon=0.2, epsilon_decay=True)
 
